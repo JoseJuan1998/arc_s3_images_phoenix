37fa70daa4d1771c7171b2cc672a2d8fa8ec796f
Juan Alcantara <juan@cordage.io>
2022-05-14 19:14:48 -0500

    Updates for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ import Config
 config :teacher, TeacherWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "https://stark-peak-45364.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
